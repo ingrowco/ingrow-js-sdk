31fd6dee755c4a3b8edb9358bafbe09eec28e297
fingerpich <zarei.bs@gmail.com>
2021-06-28 19:22:17 +0430

    fixed problems and splited code into some modules to make the codes clear

---
+++ src/anonymous-id.js
@@ -0,0 +1,36 @@
+function getRandomString(len) {
+  let s = ""
+  while (s.length < len)
+    s += Math.random().toString(36).substr(2, len - s.length)
+  return s
+}
+
+function saveValue(key, value) {
+  if (window.localStorage) {
+    window.localStorage.setItem(key, value)
+  } else {
+    let date = new Date();
+    date.setTime(date.getTime() + (315360000000)); //10 years
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = `${key}=${value}; ${expires}`;
+  }
+}
+function getValue(key) {
+  if (window.localStorage) {
+    return window.localStorage.getItem(key)
+  } else {
+    return document.cookie.match('(^|;)\\s*' + key + '\\s*=\\s*([^;]+)')?.pop() || ''
+  }
+}
+
+export function getCreatedAnonymousId() {
+  const ingrowKeyName = "ingrow_events_anonymous_id"
+  let anonymousId = getValue(ingrowKeyName)
+
+  if (!anonymousId) {
+    anonymousId = getRandomString(32)
+    saveValue(ingrowKeyName, anonymousId)
+  }
+
+  return anonymousId
+}
