18b8bdd78a7e40ce20f45d1975e7b03b2357c304
fingerpich <zarei.bs@gmail.com>
2021-07-12 00:55:44 +0430

    updated d.ts, set arguments optional

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
 declare interface Options {
-  sendDeviceInfo: boolean,
-  done: (err: object, response: object) => void
+  sendDeviceInfo?: boolean,
+  done?: (err?: object, response?: object) => void
 }
 
 declare class Ingrow {
   constructor(apiKey: string, projectID: string, userId?: string);
   setUserID(userID: string): void;
-  sendEvent(stream: string, data: object, options: Options): void;
+  sendEvent(stream: string, data: object, options?: Options): void;
 }
 
 export default Ingrow
